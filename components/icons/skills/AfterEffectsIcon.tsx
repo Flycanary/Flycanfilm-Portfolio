dc10f240fd04883cca7b98f60344f0c6961247a3
Flycanary <veo3milegabhai@gmail.com>
2025-08-24 01:01:13 +0530

    latest ae

--- components/icons/skills/AfterEffectsIcon.tsx
+++ components/icons/skills/AfterEffectsIcon.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const AfterEffectsIcon: React.FC<{ className?: string }> = ({ className }) => (
-    <svg className={className} viewBox="0 0 100 98" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="100" height="98" rx="22" fill="#1F0120"/>
-        <path d="icons8-adobe-after-effects-48.png" fill="#D85DEB"/>
-    </svg>
+  <svg
+    className={className}
+    viewBox="0 0 100 98"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="100" height="98" rx="22" fill="#1F0120" />
+    <image href="icons8-adobe-after-effects-48.png" x="25" y="25" height="48" width="48" />
+  </svg>
 );
 
 export default AfterEffectsIcon;
