f4e6d02c967c7e40be09f6331a488be1a5ba99c4
Flycanary <veo3milegabhai@gmail.com>
2025-08-24 00:55:21 +0530

    Latest changes AE

--- components/icons/skills/AfterEffectsIcon.tsx
+++ components/icons/skills/AfterEffectsIcon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AfterEffectsIcon: React.FC<{ className?: string }> = ({ className }) => (
     <svg className={className} viewBox="0 0 100 98" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect width="100" height="98" rx="22" fill="#1F0120"/>
-        <path d="M29.5 73L21 25H31L36 47.5L41.5 25H51L56.5 47.5L61.5 25H71L62.5 73H51L46.5 53.5L41 73H29.5Z" fill="#D85DEB"/>
+        <path d="icons8-adobe-after-effects-48.png" fill="#D85DEB"/>
     </svg>
 );
 
